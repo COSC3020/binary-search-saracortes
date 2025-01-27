00b1fd10de1d66c2ece39d1924c40a0fbb82a691
Sara Cortes <80413932+saracortes@users.noreply.github.com>
2025-01-27 22:16:21 +0000

    revised binary search

--- code.js
+++ code.js
@@ -1,22 +1,24 @@
 function binarySearch(list, element) {
-    if (list.length == 0) {
-        return -1; //List has nothing in it
+    if (list.length === 0) {
+        return -1; // List is empty
     }
-    var low = 0;
-    var high = list.length-1;
-    while (low <= high) {
-        const mid = (low + high) / 2; 
-        // Used help from Chat GPT to figure out how to convert it to an integer
-        const midIndex = low + high >> 1;
 
-        if (list[midIndex] == element) {
-          return midIndex;
+    let low = 0;
+    let high = list.length - 1;
+
+    while (low <= high) {
+
+        //Used help from Chat GPT to figure out how to use bitwise shifting
+        const midIndex = (low + high) >> 1;
+
+        if (list[midIndex] === element) {
+            return midIndex; 
         } else if (list[midIndex] < element) {
-          low = midIndex + 1;
+            low = midIndex + 1; 
         } else {
-          high = midIndex - 1;
+            high = midIndex - 1;
         }
-      }
-    
-      return -1; // List doesn't have the element we are looking for
+    }
+
+    return -1; // The list didn't have the element we were looking for
 }
