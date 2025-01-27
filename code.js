c02dd11d2d8faba3b15b05a09c1cbd9d3dcce647
Sara Cortes <80413932+saracortes@users.noreply.github.com>
2025-01-27 22:08:57 +0000

    finished binary search code

--- code.js
+++ code.js
@@ -1,3 +1,22 @@
 function binarySearch(list, element) {
-    return -1;
+    if (list.length == 0) {
+        return -1; //List has nothing in it
+    }
+    var low = 0;
+    var high = list.length-1;
+    while (low <= high) {
+        const mid = (low + high) / 2; 
+        // Used help from Chat GPT to figure out how to convert it to an integer
+        const midIndex = low + high >> 1;
+
+        if (list[midIndex] == element) {
+          return midIndex;
+        } else if (list[midIndex] < element) {
+          low = midIndex + 1;
+        } else {
+          high = midIndex - 1;
+        }
+      }
+    
+      return -1; // List doesn't have the element we are looking for
 }
