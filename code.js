2284913328f989655f4b30e6f323f502fd7e4646
Sara Cortes <80413932+saracortes@users.noreply.github.com>
2025-01-28 11:58:57 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,15 +1,14 @@
 function binarySearch(list, element) {
   let low = 0;
   let high = list.length - 1;
-  let result = -1;
+  let result = - 1;
 
   while (low <= high) {
-    //used help from chat gpt to figure out how to round to nearest int
-    const midIndex = Math.floor((low + high) / 2);
+    const midIndex = Math.floor((low + high) / 2); //used help from chat gpt to figure out how to round to nearest int
 
     if (list[midIndex] === element) {
-      result = midIndex; // Record the current index
-      high = midIndex - 1; // Continue searching in the left half for the first occurrence
+      result = midIndex;
+      high = midIndex - 1;
     } else if (list[midIndex] < element) {
       low = midIndex + 1;
     } else {
